dd56552dc98fa69893df097dcecdc6e1d7e26591
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-01-05 10:34:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ update_channel = os.environ.get("update_channel")
 status_message_id = int(os.environ.get("status_message_id"))
 api_id = int(os.environ.get("api_id"))
 api_hash = os.environ.get("api_hash")
-user_client = Client(session_name=str(user_session_string))#, api_id=api_id, api_hash=api_hash)
+user_client = Client(session_name=str(user_session_string), api_id=api_id, api_hash=api_hash)
 
 
 def main():
