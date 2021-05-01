076dd3b57127d3aa7248048f222ddef07497378e
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-01 13:17:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pyrogram import Client
 user_session_string = os.environ.get("user_session_string")
 bots = [i.strip() for i in os.environ.get("bots").split(' ')]
 update_channel = os.environ.get("update_channel")
-status_message_ids = os.environ.get("status_message_id")
+status_message_ids = [int(i.strip()) for i in os.environ.get("status_message_id").split(' ')]
 api_id = int(os.environ.get("api_id"))
 api_hash = os.environ.get("api_hash")
 user_client = Client(session_name=str(user_session_string), api_id=api_id, api_hash=api_hash)
